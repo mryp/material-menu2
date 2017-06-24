3e7b78511b53a3070337e6c757a43c0fcd44d63a
MRY <mry@poringsoft.net>
2017-06-24 21:12:12 +0900

    画面サイズでメニュー表示を切り替えるように変更

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit, HostListener } from '@angular/core';
 import { MdSidenav } from '@angular/material';
 
 @Component({
@@ -6,9 +6,14 @@ import { MdSidenav } from '@angular/material';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  widthTablet = 768;
+
   @ViewChild('sidenav') sidenav: MdSidenav;
+  sidenaviMode = 'over';
+  sidenaviOpened = false;
+  sidenaviIconVisible = true;
 
   naviLinkList = [
     { title: 'メニューアイテム', link: "/page", },
@@ -78,6 +83,27 @@ export class AppComponent {
     { title: 'メニューアイテム', link: "/page", },
     { title: 'メニューアイテム', link: "/page", },
   ];
+
+  ngOnInit() {
+    this.setSidenavi(window.innerWidth);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.setSidenavi(event.target.innerWidth);
+  }
+
+  setSidenavi(windowSize:number) {
+    if (windowSize < this.widthTablet) {
+      this.sidenaviMode = 'over';
+      this.sidenaviOpened = false;
+      this.sidenaviIconVisible = true;
+    } else {
+      this.sidenaviMode = 'side';
+      this.sidenaviOpened = true;
+      this.sidenaviIconVisible = false;
+    }
+  }
 
   sideNaviToggle() {
     this.sidenav.toggle();
